a93e4ca1005d9dba74dc0ac895b848c8d3dcf33c
Russom Teklay <teklayrussom992@gmail.com>
2022-05-04 17:07:21 +0200

    commit

---
+++ pages/index.js
@@ -0,0 +1,55 @@
+import React, {useContext} from "react";
+
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import axios from "axios";
+
+
+export default function Auth() {
+  const { username, secret,
+    setUsername,
+    setSecret} = useContext(Context);
+
+    const router = useRouter()
+
+    function onSubmit(e) {e.preventDefault()
+    if (username.length === 0 || secret.length === 0) return
+      axios.put('https://api.chatengine.io/users/',
+      { username, secret},
+      {headers:{"Private-key":'c6ab1525-8139-4500-b696-310ef4044e41'}}
+      ).then((r) => router.push("/chats"));
+    }
+  return (
+  <div className="background">
+  <div className="auth-container">
+  <form className="auth-form" onSubmit={e => onSubmit(e)}>
+   <div className="auth-title">Chat</div>
+   <div className="input-container">
+     <input
+      placeholder="Email"
+        className="text-input"
+        onChange={e => setUsername(e.target.value)}
+      />
+   </div>
+
+   <div className="input-container">
+     <input
+     type="password"
+      placeholder="Password"
+        className="text-input"
+        onChange={e => setSecret(e.target.value)}
+      />
+   </div>
+   <button
+   type="submit"
+   className="submit-button">
+     Login /Sign Up
+   </button>
+
+  </form>
+
+
+  </div>
+  </div>
+  );
+}
